70b4914590ff847cd53debcf824570fa13e242f6
Kerry <info@svillon.com>
2020-08-27 23:01:59 +0900

    Fix: firebase api

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,14 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDXHcJtXvy08mlIWjCoj0w5lyBom-nuUiI",
-  authDomain: "dr-pepper-map-club.firebaseapp.com",
-  databaseURL: "https://dr-pepper-map-club.firebaseio.com",
-  projectId: "dr-pepper-map-club",
-  storageBucket: "dr-pepper-map-club.appspot.com",
-  messagingSenderId: "636506083949",
-  appId: "1:636506083949:web:b75f877d47627746be6564",
-  measurementId: "G-3X1JCSRM1F",
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 
