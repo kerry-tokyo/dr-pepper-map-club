c7fe1a22ba554a694b1d07bf559a94d71a3cb5a8
Kerry <info@svillon.com>
2020-08-27 23:18:52 +0900

    Fix: firebase api

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,13 +6,13 @@ let db;
 let storage;
 try {
   const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+    authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+    projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.GATSBY_FIREBASE_APP_ID,
   };
 
   // リロード時にfirebaseが重複読み込みされないように制御する
