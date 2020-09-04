ac8bd8f16eb702961dc65c27060fe73a8869d469
Kerry <info@svillon.com>
2020-09-04 15:31:55 +0900

    Fix: header

--- src/components/map/CurrentMarker.tsx
+++ src/components/map/CurrentMarker.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import s from "./Map.scss";
 
-const CurrentMarker = ({ ...props }) => {
+const CurrentMarker = () => {
   return (
-    <div className={s.current__marker} {...props}>
+    <div className={s.current__marker}>
       <div className={s.pin__container}>
         <div className={s.pin}></div>
         <div className={s.pin__effect}></div>
--- src/components/map/Marker.tsx
+++ src/components/map/Marker.tsx
@@ -4,8 +4,8 @@ import s from "./Map.scss";
 
 import Icon from "../../assets/svg/marker.svg";
 
-export const Marker = ({ ...props }) => (
-  <div className={s.marker} {...props}>
+export const Marker = () => (
+  <div className={s.marker}>
     <Icon className={s.icon} />
   </div>
 );
